a1d9bae4ec66a2658ac803b51f986ab4bac40621
Jose Lopez <vashim_vogom@yahoo.com>
2024-09-21 12:46:15 -0600

    Adding Base empty iOS Project

---
+++ Moviez/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Moviez
+//
+//  Created by Lord Jose Lopez on 21/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("MovieZ App")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoviezTests/MoviezTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoviezTests.swift
+//  MoviezTests
+//
+//  Created by Lord Jose Lopez on 21/09/24.
+//
+
+import Testing
+@testable import Moviez
+
+struct MoviezTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
